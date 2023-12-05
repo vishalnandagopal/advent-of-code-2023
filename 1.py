5b116071485345919e63abbc167822911c384d67
Vishal N <git@vishalnandagopal.com>
2023-12-05 23:53:03 +0530

    day 1 part 2, with help from ahuja

--- 1.py
+++ 1.py
@@ -1,15 +1,57 @@
-import os
+from os.path import dirname
 
-with open(os.path.dirname(__file__) + "./input.txt", "r") as f:
+with open(dirname(__file__) + "./input.txt", "r") as f:
     test_cases = [line.strip() for line in f.readlines()]
 
 
-print(
-    sum(
-        map(
-            lambda case: int(list(filter(lambda x: x.isnumeric(), list(case)))[0]) * 10
-            + int(list(filter(lambda x: x.isnumeric(), list(case)))[-1]),
-            test_cases,
-        )
+words_to_num = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+]
+sumi = 0
+
+
+def f(test_case):
+    global sumi
+    low_numeric, high_numeric = None, None
+    for i, x in enumerate(test_case):
+        if x.isnumeric():
+            if low_numeric is None:
+                low_numeric = (x, i)
+            high_numeric = (x, i)
+
+    indices = list(
+        (test_case.index(w), test_case.rindex(w), str(i))
+        for i, w in enumerate(words_to_num)
+        if w in test_case
     )
-)
+
+    if len(indices) != 0:
+        low_word, high_word = min(indices), max(indices, key=lambda x: x[1])
+        if low_numeric is not None and low_numeric[1] < low_word[0]:
+            low = low_numeric[0]
+        else:
+            low = low_word[2]
+
+        if high_numeric is not None and high_numeric[1] > high_word[1]:
+            high = high_numeric[0]
+        else:
+            high = high_word[2]
+
+        test_case = low + high
+
+    else:
+        test_case = low_numeric[0] + high_numeric[0]
+    sumi += int(test_case)
+    return int(test_case)
+
+
+print(sum(map(f, test_cases)))
---
+++ 1_2.py
@@ -0,0 +1,77 @@
+from os.path import dirname
+
+with open(dirname(__file__) + "./input.txt", "r") as f:
+    test_cases = [line.strip() for line in f.readlines()]
+
+
+words_to_num = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+]
+
+count = 0
+
+for _, test_case in enumerate(test_cases):
+    i = 0
+    while i < len(test_case):
+        j = i + 3
+        while j < len(test_case) + 1:
+            if test_case[i:j] in words_to_num:
+                # print(test_case)
+                test_case = (
+                    test_case[:i]
+                    + str(words_to_num.index(test_case[i:j]))
+                    + test_case[j:]
+                )
+                # print(test_case)
+                break
+            j += 1
+        i += 1
+    test_cases[_] = test_case
+
+print(test_cases[-30:])
+
+
+def a(test_case):
+    _ = int(list(filter(lambda x: x.isnumeric(), list(test_case)))[0]) * 10 + int(
+        list(filter(lambda x: x.isnumeric(), list(test_case)))[-1]
+    )
+    print(_)
+    return _
+
+
+# print(
+#     sum(
+#         map(
+#             lambda test_case: a(test_case),
+#             test_cases,
+#         )
+#     )
+# )
+
+# Alternate multi-line, readable solution
+
+sumi = 0
+
+for test_case in test_cases:
+    _ = ""
+    # _ is a string containing all the integers in one specific test test_case
+    
+    for x in test_case:
+        if x.isnumeric():
+            _ += x
+    
+    # This steps add the (first integer in the test test_case * 10 + last integer in the test test_case) to the overall sum, if _ is not empty. It can use first and last elements of _ directly since _ consists of just numbers of x and is in the same order
+    sumi += int(_[0]) * 10 + int(_[-1]) if _ else 0
+    print(f"{sumi} - {test_case}")
+
+print(sumi)
+
